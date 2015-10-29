d8f2d7606697630e97291d0a974d48b218efde2d
Your Name <neymank@gilmour.org>
2015-10-29 09:01:43 -0400

    Pigo commit

--- Pigo.py
+++ Pigo.py
@@ -1,12 +1,31 @@
-#from gopigo import*
-
+from gopigo import*
+import time
 
 __author__ = 'gilmour'
 
 class Pigo:
 
+    isMoving = False
+    servoPos = 90
+
     def __init__(self):
         print "I am alive."
 
+    def stop(self):
+        self.isMoving = False
+        while stop() !=1
+            time.sleep(.1)
+            print "Whoops, sorry boss. Can't stop."
+    def fwd(self):
+        self.isMoving = True
+        while fwd() != 1:
+            time.slepp(.1)
+            print "Can't do the vroom vroom"
+
+
+
 tina = Pigo()
+tina.fwd()
+tina.sleep(2)
+tina.stop()
 
